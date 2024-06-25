dc54f9d7e4cefd6acf40d7f2fbe9cc2503c09cd8
Serge Zakhary <grimglance@gmail.com>
2024-06-25 17:06:19 +0700

    added ConditionalCompilationTask

---
+++ ConditionalCompilationTask/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConditionalCompilationTask;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+#if DEBUG
+
+        #region DebugPart
+
+        Console.WriteLine("Debug mode");
+
+        #endregion
+#else
+        #region ReleasePart
+
+        Console.WriteLine("Release mode");
+
+        #endregion
+#endif
+        Console.ReadKey();
+    }
+}
