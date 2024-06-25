5111a9587b1dc6babee9e653b82dd5b1810ab6f2
Serge Zakhary <grimglance@gmail.com>
2024-06-25 14:53:31 +0700

    added config task

---
+++ ConfigTask/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Configuration;
+
+namespace ConfigTask;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(ConfigurationManager.AppSettings["url"]);
+    }
+}
