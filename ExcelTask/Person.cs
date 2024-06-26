90506e7664681acb1256fb376664b95c40fde5e3
Serge Zakhary <grimglance@gmail.com>
2024-06-27 03:10:27 +0700

    added excel task

---
+++ ExcelTask/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace ExcelTask;
+
+public class Person(string FirstName, string LastName, int Age, string PhoneNumber)
+{
+    public string FirstName { get; set; } = FirstName;
+
+    public string LastName { get; set; } = LastName;
+
+    public int Age { get; set; } = Age;
+
+    public string PhoneNumber { get; set; } = PhoneNumber;
+}
---
+++ ExcelTask/Program.cs
@@ -0,0 +1,42 @@
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Style;
+
+namespace ExcelTask;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var persons = new List<Person>
+        {
+            new("Алексей", "Алексеев", 31, "89999999999"),
+            new("Александр ", "Бирюков", 30, "89999999998"),
+            new("Дмитрий", "Валюхов", 29, "89999999997"),
+            new("Евгений", "Данилов", 28, "89999999996"),
+            new("Евгений", "Журов", 27, "89999999995")
+        };
+
+        var outputFile = new FileInfo("persons.xlsx");
+        
+        if (outputFile.Exists)
+        {
+            outputFile.Delete();
+        }
+
+        using var excelPackage = new ExcelPackage(outputFile);
+        var workSheet = excelPackage.Workbook.Worksheets.Add("Persons");
+        workSheet.Cells[1, 1].Value = "FirstName";
+        workSheet.Cells[1, 2].Value = "LastName";
+        workSheet.Cells[1, 3].Value = "Age";
+        workSheet.Cells[1, 4].Value = "PhoneNumber";
+        
+        workSheet.Cells[2,1].LoadFromCollection(persons);
+        workSheet.Cells.AutoFitColumns();
+        var firstRow = workSheet.Row(1);
+        firstRow.Style.Fill.PatternType = ExcelFillStyle.LightGray;
+        firstRow.Style.Font.Bold = true;
+        excelPackage.Save();
+    }
+}
