a7ef1bbc031ecd83ae7f117ed87f79bf27d10db0
Serge Zakhary <grimglance@gmail.com>
2024-07-13 04:01:01 +0700

    fixed nullable

--- JsonTask/Currency.cs
+++ JsonTask/Currency.cs
@@ -3,8 +3,8 @@
 public class Currency
 {
     public string? Code { get; set; }
-    
+
     public string? Name { get; set; }
 
-    public required string Symbol { get; set; }
-} 
+    public string? Symbol { get; set; }
+}
