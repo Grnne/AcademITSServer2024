e1742da3beeedca8b55f837f33c1726e0416d25f
Serge Zakhary <grimglance@gmail.com>
2024-07-14 19:48:15 +0700

    fixed tests

---
+++ XUnitTestTask/XUnitTestTask.cs
@@ -0,0 +1,29 @@
+using VectorTask;
+
+namespace XUnitTestTask;
+
+public class XUnitTestTask
+{
+    [Fact]
+    public void Reverse_Vector_ReversingVector()
+    {
+        var vector = new Vector([0, 0, 0, 1, 3]);
+        var expected = new Vector([-0, -0, -0, -1, -3]);
+
+        vector.Reverse();
+
+        Assert.Equal(vector, expected);
+    }
+
+    [Fact]
+    public void GetDifference_Vectors_ReturnsVectorsDifference()
+    {
+        var vector1 = new Vector([0, 0, 0, 1, 3]);
+        var vector2 = new Vector([1, 1, 1, 1, 1, 1, 1]);
+        var expected = new Vector([-1, -1, -1, 0, 2, -1, -1]);
+
+        var actual = Vector.GetDifference(vector1, vector2);
+
+        Assert.Equal(actual, expected);
+    }
+}
